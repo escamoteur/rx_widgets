79a8f14fcd664a9047f4b0650dd70dec7480a709
Alexander Babich <oleksandr.babich@fortegrp.com>
2020-09-16 18:56:03 +0300

    WidgetSelector - Made onTrue, onFalse optional. Blank widget is used if null. This is usable if you need to put custom widget only for one branch
    RxCommandBuilder
    - Made stateless widget
    - Made possible to build with blank of Placeholder and Error. Blank widgets used in that case
    - Made possible to build with blank Busy. Also used platform parameter to build progress indicator if busy is null
    
    RxCommandExecutable
    - New widget. Lets you wrap your widget and give functions which will be run once new data is emitted in streams. This way you may keep your widgets stateless and clean.

--- lib/src/rx_comand_builder.dart
+++ lib/src/rx_comand_builder.dart
@@ -1,33 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:rx_command/rx_command.dart';
 import 'package:rx_widgets/src/builder_functions.dart';
 
-/// Spinner/Busyindicator that reacts on the output of a `Stream<CommandResult<T>>`. It's made especially to work together with
-/// `RxCommand` from the `rx_command`package.
-/// it starts running as soon as an item with  `isExecuting==true` is received
-/// until `isExecuting==true` is received.
-/// To react on other possible states (`data, nodata, error`) that can be emitted it offers three option `Builder` methods
-class RxCommandBuilder<T> extends StatefulWidget {
+/// Spinner/Busy indicator that reacts on the output of a `Stream<CommandResult<T>>`.
+/// It's made especially to work together with `RxCommand` from the `rx_command`package.
+/// it starts running as soon as an item with `isExecuting==true` is received  until `isExecuting==true` is received.
+/// To react on other possible states (`data, no data, error`) that can be emitted it offers three option `Builder` methods.
+class RxCommandBuilder<T> extends StatelessWidget {
   final Stream<CommandResult<T>> commandResults;
   final RxBuilder<T> dataBuilder;
   final ErrorBuilder<Exception> errorBuilder;
   final BusyBuilder busyBuilder;
   final PlaceHolderBuilder placeHolderBuilder;
-
   final TargetPlatform platform;
 
   /// Creates a new `RxCommandBuilder` instance
   /// [commandResults] : `Stream<CommandResult<T>>` or a `RxCommand<T>` that issues `CommandResults`
   /// [busyBuilder] : Builder that will be called as soon as an event with `isExecuting==true`.
-  /// [dataBuilder] : Builder that will be called as soon as an event with data is received. It will get passed the `data` feeld of the CommandResult.
+  /// [dataBuilder] : Builder that will be called as soon as an event with data is received. It will get passed the `data` field of the CommandResult.
   /// If this is null a `Container` will be created instead.
   /// [placeHolderBuilder] : Builder that will be called as soon as an event with `data==null` is received.
   /// If this is null a `Container` will be created instead.
-  /// [dataBuilder] : Builder that will be called as soon as an event with an `error` is received. It will get passed the `error` feeld of the CommandResult.
+  /// [dataBuilder] : Builder that will be called as soon as an event with an `error` is received. It will get passed the `error` field of the CommandResult.
   /// If this is null a `Container` will be created instead.
   const RxCommandBuilder({
     Key key,
-    this.commandResults,
+    @required this.commandResults,
     this.platform,
     this.busyBuilder,
     this.dataBuilder,
@@ -37,50 +37,57 @@ class RxCommandBuilder<T> extends StatefulWidget {
         super(key: key);
 
   @override
-  _RxCommandBuilderState createState() {
-    return new _RxCommandBuilderState<T>();
-  }
-}
-
-class _RxCommandBuilderState<T> extends State<RxCommandBuilder<T>> {
-  @override
   Widget build(BuildContext context) {
     return StreamBuilder<CommandResult<T>>(
-      stream: widget.commandResults,
+      stream: commandResults,
       builder: (context, snapshot) {
+        CommandResult<T> item;
         if (snapshot.hasData) {
-          final item = snapshot.data;
-          return _processItem(item);
+          item = snapshot.data;
         } else if (snapshot.hasError) {
-          return widget.errorBuilder(context, snapshot.error);
+          item = CommandResult.error(snapshot.error);
         } else {
-          return widget.placeHolderBuilder(context);
+          item = const CommandResult(null, null, false);
         }
+        return _processItem(context, item);
       },
     );
   }
 
-  Widget _processItem(CommandResult<T> item) {
+  Widget _processItem(BuildContext context, CommandResult<T> item) {
+    assert(item != null);
+
     if (item.isExecuting) {
-      return widget.busyBuilder(context);
-    }
-    if (item.hasData) {
-      if (widget.dataBuilder != null) {
-        return widget.dataBuilder(context, item.data);
+      if (busyBuilder != null) {
+        return busyBuilder(context);
+      } else {
+        final spinner = (platform ?? defaultTargetPlatform == TargetPlatform.iOS)
+            ? const CupertinoActivityIndicator()
+            : const CircularProgressIndicator();
+        return Center(child: spinner);
       }
     }
 
-    if (!item.hasData && !item.hasError) {
-      if (widget.placeHolderBuilder != null) {
-        return widget.placeHolderBuilder(context);
+    if (item.hasData) {
+      if (dataBuilder != null) {
+        return dataBuilder(context, item.data);
+      } else {
+        return const SizedBox();
       }
     }
 
     if (item.hasError) {
-      if (widget.errorBuilder != null) {
-        return widget.errorBuilder(context, item.error);
+      if (errorBuilder != null) {
+        return errorBuilder(context, item.error);
+      } else {
+        return const SizedBox();
       }
     }
-    return SizedBox();
+
+    if (placeHolderBuilder != null) {
+      return placeHolderBuilder(context);
+    } else {
+      return const SizedBox();
+    }
   }
 }
